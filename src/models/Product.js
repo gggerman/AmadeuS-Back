5fa6bfac1548cac3a3e6fd36761b0dbf76c0e302
Franco Heis <franco@hotmail.com>
2021-09-02 19:39:25 -0300

    models

---
+++ src/models/Product.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+
+const productSchema = new Schema({
+    /* id, */
+
+    name: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    stock: {
+        type: Number,
+        required: true
+    },
+    brand: {
+        type: String,
+        required: true
+    },
+    categories: {
+        type: Array
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    qualification: Array,
+    //compradores: [{}],
+},
+{
+    timestamps: false,
+    versionKey: false,
+  }
+)
+
+const Product = model('Product', productSchema)
+
+module.exports = Product
