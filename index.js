3c640be8d86b87aac3d29d6bb4e58c4b2c2e03bb
gggerman <21german.garcia@gmail.com>
2021-09-09 00:54:38 -0300

    probando heroku

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const server = require('./src/app.js');
 const connectionDB  = require('./src/db.js');
 const express = require('express');
 
-const server = express();
-
 // Conexión a DB
 connectionDB()
 
@@ -11,5 +9,5 @@ server.set('port', process.env.PORT || 3001);
 
 // Corriendo el servidor
 server.listen(server.get('port'), () => {
-  console.log(`Server running on port ${app.get('port')}`); // eslint-disable-line no-console
+  console.log(`Server running on port ${server.get('port')}`); // eslint-disable-line no-console
 });
