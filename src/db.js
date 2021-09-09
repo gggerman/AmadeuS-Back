a6a1dd605698f80387b9eac5b1b7e104a8cbfa29
gggerman <21german.garcia@gmail.com>
2021-09-08 22:58:28 -0300

    probando heroku

--- src/db.js
+++ src/db.js
@@ -4,10 +4,10 @@ const { DB_URI } = process.env;
 const mongoose = require('mongoose')
 
 let connectionDB = () => {
-  mongoose.connect(DB_URI, {
+  mongoose.connect('mongodb+srv://equipo16:vamoslospibes@cluster0.5doep.mongodb.net/ecommerce?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  }) 
+  })
   mongoose.connection.on('open', _ => {
     console.log('Database connected')
   })
@@ -20,5 +20,3 @@ let connectionDB = () => {
 }
 
 module.exports = connectionDB
-
-
