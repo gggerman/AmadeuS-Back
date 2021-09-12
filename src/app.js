46b50c5a373b2d4ab83f42223c64c15c6bb43ded
gggerman <21german.garcia@gmail.com>
2021-09-12 02:10:48 -0300

    probando cors

--- src/app.js
+++ src/app.js
@@ -11,13 +11,13 @@ const server = express();
 server.name = 'API';
 
 // Middlewares
+server.use(cors());
 server.use(express.urlencoded({ extended: true, limit: '50mb' }));
 server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
-server.use(cors());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://musical-e-commerce.vercel.app'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'https://musical-e-commerce.vercel.app'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
