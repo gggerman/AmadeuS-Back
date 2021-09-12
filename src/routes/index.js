2bf7d0e36c271962088fef1b1de0b3938dce2bb2
Juan Pablo Heis <80589629+juanpabloheis@users.noreply.github.com>
2021-09-12 19:55:56 -0300

    (/app, index/) varias

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,4 @@
 const { Router } = require('express');
-
 // Importación de routers.
 const products = require('./products')
 const categories = require('./categories')
@@ -9,50 +8,12 @@ const orders = require('./orders')
 
 const router = Router();
 
-//Auth
-var jwt = require('express-jwt');
-var jwks = require('jwks-rsa');
-
-var jwtCheck = jwt({
-  secret: jwks.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: 'https://dev-0-knpzfi.us.auth0.com/.well-known/jwks.json'
-  }),
-  audience: 'http://api.musical.ecommerce',
-  issuer: 'https://dev-0-knpzfi.us.auth0.com/',
-  algorithms: ['RS256']
-});
-
-// Configurar los routers. 
-// app.use(jwtCheck);
-router.get('/user/manifestStripeCustomer', jwtCheck, function (req, res) {
-    var request = require("request");
-
-    var options = {
-        method: 'POST',
-        url: 'https://dev-0-knpzfi.us.auth0.com/oauth/token',
-        headers: { 'content-type': 'application/json' },
-        body: '{"client_id":"hdinEoO2VwjA8d9ZVTqVaovaWU5hdwEC","client_secret":"E_JGcr6c8zyRDjGETh-hsptgUfcDir_k73ThRoUdJ_IkhwfrDfLMoim_BU0_62yv","audience":"https://dev-0-knpzfi.us.auth0.com/api/v2/","grant_type":"client_credentials"}'
-    };
-
-    request(options, function (error, response, body) {
-        if (error) throw new Error(error);
-
-        console.log(body);
-    });
-});
-
-
 // Configurar los routers.
 router.use('/products', products)
 router.use('/categories', categories)
 router.use('/users', users)
 router.use('/mercadopago', mercadopago)
 router.use('/orders', orders)
-
-
 
 module.exports = router;
 
